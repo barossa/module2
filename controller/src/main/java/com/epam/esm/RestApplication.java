e9e3b0e3a948b4b845858d389e8c2f50b41155f9
Anton Prokopovich <antoxaunited@gmail.com>
2022-03-21 13:41:15 +0300

    Support for external containers

--- controller/src/main/java/com/epam/esm/RestApplication.java
+++ controller/src/main/java/com/epam/esm/RestApplication.java
@@ -2,10 +2,17 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestApplication.class);
     }
 }
